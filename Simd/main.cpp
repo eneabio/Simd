bbabddb92f9b2fc1b5aee1ccba2706ea50a2875c
Enea Bionda <eneabio1974@gmail.com>
2013-02-19 15:26:36 +0100

    Executed test with SIMD and without SIMD in OSX.

--- Simd/main.cpp
+++ Simd/main.cpp
@@ -13,29 +13,68 @@
 #include <xmmintrin.h> // Contain the SSE compiler intrinsics
 //#include <malloc.h>
 #include "performance.h"
+#include <math.h>
+#include <time.h>       /* time */
 
-void myssefunction
-(
- float* pArray1,                   // [in] first source array
- float* pArray2,                   // [in] second source array
- float* pResult,                   // [out] result array
- int nSize                         // [in] size of all arrays
- )
+void myssefunctionSIMD ( float* pArray1, float* pArray2, float* pResult, int nSize);
+void myssefunction     ( float* pArray1, float* pArray2, float* pResult, int nSize);
+void * malloc_simd     (const size_t size);
+void * free_simd       (void * p);
+
+int main(int argc, char *argv[])
 {
-	const __m128 m0_5 = _mm_set_ps1(0.5f);        // m0_5[0, 1, 2, 3] = 0.5
-	for (size_t index = 0; index < nSize; index += 4)
-	{
-		__m128 pSrc1 = _mm_load_ps(pArray1 + index); // load 4 elements from memory into SSE register
-		__m128 pSrc2 = _mm_load_ps(pArray2 + index); // load 4 elements from memory into SSE register
+	using namespace performance;
+	
+	for (int j = 0; j <= 8; ++j) {
+		int ARRAY_SIZE = 4 * pow (10.f, j);  //original was 10
+		float* m_fArray1 = (float*) malloc_simd(ARRAY_SIZE * sizeof(float));
+		float* m_fArray2 = (float*) malloc_simd(ARRAY_SIZE * sizeof(float));
+		float* m_fArray3 = (float*) malloc_simd(ARRAY_SIZE * sizeof(float));
+	
+		printf("# cycles: %d\n", ARRAY_SIZE);
+		{
+			/* initialize random seed: */
+			srand (time(NULL));
+			for (int i = 0; i < ARRAY_SIZE; ++i)
+			{
+				//original
+				m_fArray1[i] = ((float)rand())/RAND_MAX;
+				m_fArray2[i] = ((float)rand())/RAND_MAX;
+				//m_fArray1[i] = i + 1.f;
+				//m_fArray2[i] = i + 2.f;
+			}
+			uint64_t start = Start();
+			myssefunction(m_fArray1 , m_fArray2 , m_fArray3, ARRAY_SIZE);
+			uint64_t stop = Stop();
+			double time = ExecutionTime(start, stop);
+			printf("Execution time without SIMD = %e \n", time);
+		}
 		
-		__m128 m1   = _mm_mul_ps(pSrc1, pSrc1);        // m1 = *pSrc1 * *pSrc1
-		__m128 m2   = _mm_mul_ps(pSrc2, pSrc2);        // m2 = *pSrc2 * *pSrc2
-		__m128 m3   = _mm_add_ps(m1, m2);                // m3 = m1 + m2
-		__m128 m4   = _mm_sqrt_ps(m3);                   // m4 = sqrt(m3)
-		__m128 pDest  = _mm_add_ps(m4, m0_5);          // pDest = m4 + 0.5
+		{
+			/* initialize random seed: */
+			srand (time(NULL));
+			for (int i = 0; i < ARRAY_SIZE; ++i)
+			{
+				//original
+				m_fArray1[i] = ((float)rand())/RAND_MAX;
+				m_fArray2[i] = ((float)rand())/RAND_MAX;
+				//m_fArray1[i] = i + 1.f;
+				//m_fArray2[i] = i + 2.f;
+			}
+			uint64_t startSimd = Start();
+			myssefunctionSIMD(m_fArray1 , m_fArray2 , m_fArray3, ARRAY_SIZE);
+			uint64_t stopSimd = Stop();
+			double timeSimd = ExecutionTime(startSimd, stopSimd);
+			printf("Execution time with SIMD = %e \n", timeSimd);
+		}
+	
+		free(m_fArray1);
+		free(m_fArray2);
+		free(m_fArray3);
 		
-		_mm_store_ps(pResult + index, pDest); // store 4 elements from SSE register to memory.
 	}
+	
+	return 0;
 }
 
 void * malloc_simd(const size_t size)
@@ -64,33 +103,40 @@ void * free_simd(void * p)
 #endif
 }
 
-int main(int argc, char *argv[])
+void myssefunctionSIMD
+(
+ float* pArray1,                   // [in] first source array
+ float* pArray2,                   // [in] second source array
+ float* pResult,                   // [out] result array
+ int nSize                         // [in] size of all arrays
+ )
 {
-	using namespace performance;
-	
-	uint64_t start = Start();
-	
-	int ARRAY_SIZE = 10;
-	float* m_fArray1 = (float*) malloc_simd(ARRAY_SIZE * sizeof(float));
-	float* m_fArray2 = (float*) malloc_simd(ARRAY_SIZE * sizeof(float));
-	float* m_fArray3 = (float*) malloc_simd(ARRAY_SIZE * sizeof(float));
-	
-	for (int i = 0; i < ARRAY_SIZE; ++i)
+	const __m128 m0_5 = _mm_set_ps1(0.5f);        // m0_5[0, 1, 2, 3] = 0.5
+	for (size_t index = 0; index < nSize; index += 4)
 	{
-		m_fArray1[i] = ((float)rand())/RAND_MAX;
-		m_fArray2[i] = ((float)rand())/RAND_MAX;
+		__m128 pSrc1 = _mm_load_ps(pArray1 + index); // load 4 elements from memory into SSE register
+		__m128 pSrc2 = _mm_load_ps(pArray2 + index); // load 4 elements from memory into SSE register
+		
+		__m128 m1   = _mm_mul_ps(pSrc1, pSrc1);        // m1 = *pSrc1 * *pSrc1
+		__m128 m2   = _mm_mul_ps(pSrc2, pSrc2);        // m2 = *pSrc2 * *pSrc2
+		__m128 m3   = _mm_add_ps(m1, m2);                // m3 = m1 + m2
+		__m128 m4   = _mm_sqrt_ps(m3);                   // m4 = sqrt(m3)
+		__m128 pDest  = _mm_add_ps(m4, m0_5);          // pDest = m4 + 0.5
+		
+		_mm_store_ps(pResult + index, pDest); // store 4 elements from SSE register to memory.
 	}
-	
-	myssefunction(m_fArray1 , m_fArray2 , m_fArray3, ARRAY_SIZE);
-	
-	free(m_fArray1);
-	free(m_fArray2);
-	free(m_fArray3);
-	
-	uint64_t stop = Stop();
-	double time = ExecutionTime(start, stop);
-	printf("Execution time = %f", time);
-	
-	return 0;
 }
 
+void myssefunction
+(
+ float* pArray1,                   // [in] first source array
+ float* pArray2,                   // [in] second source array
+ float* pResult,                   // [out] result array
+ int nSize                         // [in] size of all arrays
+ )
+{
+	for (size_t i = 0; i < nSize; i += 1)
+	{
+		pResult[i] = sqrtf((pArray1[i] * pArray1[i]) + (pArray2[i] * pArray2[i])) + 0.5f;
+	}
+}
