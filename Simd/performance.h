0726c45c3369fa441fe1a80729b8d07adf658b36
Enea Bionda <eneabio1974@gmail.com>
2013-02-19 15:26:36 +0100

    Executed test with SIMD and without SIMD in OSX.

--- Simd/performance.h
+++ Simd/performance.h
@@ -9,7 +9,7 @@
 namespace performance {
 	uint64_t   Start          ();
 	uint64_t   Stop           ();
-	double   ExecutionTime  (uint64_t start, uint64_t stop);
+	double     ExecutionTime  (uint64_t start, uint64_t stop);
 }
 
 #endif
